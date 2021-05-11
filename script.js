af811020533697bc6757e55da0a6b81b39ec6b01
Shubham Raj <71963532+Shubhamrajgithub@users.noreply.github.com>
2021-05-11 11:00:05 +0530

    tea
    good

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if(this.scrollY > 20)
+    $(".navbar").addClass("sticky");
+    else
+    $(".navbar").removeClass("sticky");
+  })
+})
